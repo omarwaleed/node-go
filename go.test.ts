54b14e82c9762f5c16f015303228a81cf8c87549
omarwaleed <omarpersonalprofile@gmail.com>
2023-01-20 10:42:42 +0200

    init commit with v1.0.0

---
+++ go.test.ts
@@ -0,0 +1,49 @@
+import go from '.';
+
+describe('go', () => {
+  it('should spawn a new thread and run it returning the result of the function in a promise', async () => {
+    const result = await go((a, b) => a + b, 1, 2);
+    expect(result).toBe(3);
+  });
+
+  it('should spawn a new thread with a function made up of more than 2 arguments', async () => {
+    const result = await go((a, b, c, d) => a + b + c + d, 1, 2, 3, 4);
+    expect(result).toBe(10);
+  });
+
+  it('should spawn a new thread with a function made up of more spread over multiple lines', async () => {
+    const result = await go((a, b, c, d) => {
+      return a + b + c + d;
+    }, 1, 2, 3, 4);
+    expect(result).toBe(10);
+  });
+
+  it("should spawn a new thread with a function that doesn't return anything", async () => {
+    const result = await go(() => {
+      console.log('Hello world!');
+    });
+    expect(result).toBe(undefined);
+  })
+
+  it("should spawn a new thread with a function that does nothing and not run it if execution ends", () => {
+    const fn = jest.fn();
+    go(() => {
+      setTimeout(() => {
+        fn();
+      }, 250);
+    }).catch(console.error);
+    expect(fn).not.toHaveBeenCalled();
+  })
+
+  it("should throw an error if the first argument is not a function", async () => {
+    let error, value;
+    try {
+      // @ts-ignore
+      value = await go(1, 2, 3, 4);
+    } catch (err) {
+      error = err;
+    }
+    expect(error).toBeDefined();
+    expect(value).toBeUndefined();
+  });
+});
---
+++ index.ts
@@ -0,0 +1,40 @@
+import * as fs from 'fs';
+import path from 'path';
+import { Worker } from 'worker_threads';
+
+async function go(fn: (..._: any) => any, ...args: any) {
+  if(typeof fn !== 'function') {
+    throw new Error('fn is not a function');
+  }
+  const fnData = fn.toString();
+  return new Promise((resolve, reject) => {
+    const workerFileName = `worker-${new Date().valueOf()}.js`;
+    const workerFilePath = path.resolve(__dirname, workerFileName);
+    fs.writeFileSync(workerFilePath, `
+      const { parentPort } = require('worker_threads');
+      const fn = ${fnData};
+      parentPort.on('message', (message) => {
+        const result = fn(...message);
+        parentPort.postMessage(result);
+      });
+    `.split("\t").join("").split("  ").join(""));
+    const worker = new Worker(workerFilePath);
+    worker.on('message', (result) => {
+      resolve(result);
+      worker.terminate();
+    });
+    worker.on('error', (err) => {
+      reject(err);
+      worker.terminate();
+    });
+    worker.on('exit', (code) => {
+      fs.unlinkSync(workerFilePath);
+      if(code !== 0) {
+        reject(new Error('Worker stopped with exit code ' + code));
+      }
+    });
+    worker.postMessage(args);
+  });
+}
+
+export default go;
